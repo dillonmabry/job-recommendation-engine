4c2ac0f35a784c5f8de5ec9f154abd88c9948399
root <root@raspberrypi>
2018-07-23 21:36:40 -0400

    Change API to PUT remove GET for upload

--- backend/app.py
+++ backend/app.py
@@ -11,22 +11,21 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['PUT'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return 'No file present'
-        file = request.files['file']
-        if file.filename == '':
-            return 'No selected file'
-        if file and allowed_file(file.filename):
-            keywords = extract_keywords(file)
-            # mail queue for tasks
-            task_id = len(TASKS)
-            TASKS[task_id] = scrape_and_mail.delay(keywords)
-            response = {'result': task_id}
-            return jsonify(response)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return 'No file present'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file and allowed_file(file.filename):
+        keywords = extract_keywords(file)
+        # mail queue for tasks
+        task_id = len(TASKS)
+        TASKS[task_id] = scrape_and_mail.delay(keywords)
+        response = {'result': task_id}
+        return jsonify(response)
 
 @app.route('/', methods=['GET'])
 def list_tasks():
